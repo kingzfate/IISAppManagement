a4a47678fab46045ccc3f7649bfd3d880e24e19c
Авраменко Никита Александрович <NikAvrggl@gmail.com>
2020-12-01 11:28:39 +0400

    Добавьте файлы проекта.

---
+++ IISAppManagement/Main.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace IISAppManagement
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.notifyIconMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIconMain
+            // 
+            this.notifyIconMain.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconMain.Icon")));
+            this.notifyIconMain.Text = "notifyIconMain";
+            this.notifyIconMain.Visible = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 191);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowInTaskbar = false;
+            this.Text = "IISAppManagement";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconMain;
+    }
+}
+
---
+++ IISAppManagement/Main.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace IISAppManagement
+{
+    /// <summary>
+    /// Main class without form
+    /// </summary>
+    public partial class Main : Form
+    {
+        private readonly ServerManager mgr = new ServerManager();
+
+        public Main()
+        {
+            InitializeComponent();
+            SettingNotifyIcon();
+        }
+
+        /// <summary>
+        /// Setting up the context menu in the application tray
+        /// </summary>
+        private void SettingNotifyIcon()
+        {
+            notifyIconMain.ContextMenuStrip = new ContextMenuStrip();
+
+            foreach (Site site in mgr.Sites)
+            {
+                notifyIconMain.ContextMenuStrip.Items.Add(site.Name, null, Reboot_Click);
+            }
+            notifyIconMain.ContextMenuStrip.Items.Add("Exit", null, AppExit_Click);
+        }
+
+        /// <summary>
+        /// Reboot instance. Pool and application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Reboot_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+
+            mgr.Sites[item.Text].Stop();
+            mgr.ApplicationPools[item.Text].Stop();
+            Thread.Sleep(2000);
+            mgr.Sites[item.Text].Start();
+            mgr.ApplicationPools[item.Text].Start();
+        }
+
+        /// <summary>
+        /// Unload managers and close the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AppExit_Click(object sender, EventArgs e) 
+        {
+            mgr.Dispose();
+            System.Windows.Forms.Application.Exit(); 
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x80;  // Turn on WS_EX_TOOLWINDOW
+                return cp;
+            }
+        }
+    }
+}
