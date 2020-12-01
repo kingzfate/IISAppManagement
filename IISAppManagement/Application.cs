118624583c2c66ab2c75992639812b818a7b0e92
Авраменко Никита Александрович <NikAvrggl@gmail.com>
2020-12-01 16:22:18 +0400

    Added start, stop and restart buttons

--- IISAppManagement/Application.cs
+++ IISAppManagement/Application.cs
@@ -12,6 +12,9 @@ namespace IISAppManagement
     {
         private readonly NotifyIcon trayIcon = new NotifyIcon();
         private readonly IISManager iisMgr = new IISManager();
+        private const string runApp = "Run";
+        private const string stopApp = "Stop";
+        private const string rebootApp = "Reboot";
 
         public Application()
         {
@@ -23,28 +26,51 @@ namespace IISAppManagement
         /// </summary>
         private void SettingsNotify()
         {
+            trayIcon.ContextMenuStrip = new ContextMenuStrip();
             trayIcon.Icon = Resources.main;
             trayIcon.Visible = true;
-            trayIcon.ContextMenuStrip = new ContextMenuStrip();
+
+            ToolStripMenuItem Run = new ToolStripMenuItem { Name = runApp, Text = "Запустить" };
+            ToolStripMenuItem Stop = new ToolStripMenuItem { Name = stopApp, Text = "Остановить" };
+            ToolStripMenuItem Reboot = new ToolStripMenuItem { Name = rebootApp, Text = "Перезагрузка" };
+            Run.Click += InstanceContext_Click;
+            Stop.Click += InstanceContext_Click;
+            Reboot.Click += InstanceContext_Click;
 
             foreach (Site site in iisMgr.GetSites())
             {
-                trayIcon.ContextMenuStrip.Items.Add(site.Name, null, Reboot_Click);
+                ToolStripMenuItem newElement = new ToolStripMenuItem { Name = site.Name, Text = site.Name };
+                newElement.DropDownItems.AddRange(new ToolStripItem[] { Run, Stop, Reboot });
+                trayIcon.ContextMenuStrip.Items.Add(newElement);
             }
             trayIcon.ContextMenuStrip.Items.Add("Exit", null, AppExit_Click);
         }
 
         /// <summary>
-        /// Reboot instance. Pool and application
+        /// Perform tasks depending on the selected context
+        /// Start application, 
+        /// Stop application, 
+        /// Reload application
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Reboot_Click(object sender, EventArgs e)
+        private void InstanceContext_Click(object sender, EventArgs e)
         {
             ToolStripMenuItem item = sender as ToolStripMenuItem;
 
-            iisMgr.StopApp(item.Text);
-            iisMgr.StartApp(item.Text);
+            switch (item.Name)
+            {
+                case runApp:
+                    iisMgr.StartApp(item.OwnerItem.Text);
+                    break;
+                case stopApp:
+                    iisMgr.StopApp(item.OwnerItem.Text);
+                    break;
+                case rebootApp:
+                    iisMgr.StopApp(item.OwnerItem.Text);
+                    iisMgr.StartApp(item.OwnerItem.Text);
+                    break;
+            };
         }
 
         /// <summary>
